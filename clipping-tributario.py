4b771525e7583c731c3e721274fe49fcf85356f0
Leonardo Araujo <mendes.lnr@gmail.com>
2020-02-06 11:21:47 -0300

    handling errors

--- clipping-tributario.py
+++ clipping-tributario.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from datetime import date
-from datetime import timedelta
+from datetime import date, timedelta
 import bs4, ezgmail, os, re, requests
 
 
+lista_erros = []
 data_ontem = date.today() - timedelta(days=1)
 
 valor_dados_1 = {'nome': 'Valor Econômico', 'link': 'https://valor.globo.com/busca/?q=tributo&order=recent&page=1&from=now-1d'}
@@ -44,29 +44,33 @@ def formata(noticias, portal):
 def busca_sacha(portal, seletores):
     dicionario = {'titulo': [], 'desc': [], 'link': [], 'outstring': ''}
 
-    res = requests.get(portal['link'])
-    res.raise_for_status()
+    try:
+        res = requests.get(portal['link'])
+        res.raise_for_status()
 
-    sopa = bs4.BeautifulSoup(res.text, 'html.parser')
-    sopa_titulo = sopa.select(seletores['titulo'])
-    sopa_link = sopa.select(seletores['link'])
+        sopa = bs4.BeautifulSoup(res.text, 'html.parser')
+        sopa_titulo = sopa.select(seletores['titulo'])
+        sopa_link = sopa.select(seletores['link'])
 
-    titulo_materia = sopa_titulo[0].getText().strip()
-    link_materia = sopa_link[0].get('href')
+        titulo_materia = sopa_titulo[0].getText().strip()
+        link_materia = sopa_link[0].get('href')
 
-    res = requests.get(link_materia)
-    res.raise_for_status()
+        res = requests.get(link_materia)
+        res.raise_for_status()
 
-    sopa = bs4.BeautifulSoup(res.text, 'html.parser')
-    sopa_data = sopa.select(seletores['data'])
+        sopa = bs4.BeautifulSoup(res.text, 'html.parser')
+        sopa_data = sopa.select(seletores['data'])
 
-    data_materia = sopa_data[0].getText()
-    if data_materia == data_ontem.strftime('%d/%m/%Y'):
-        dicionario['titulo'].append('Novo Informativo')
-        dicionario['desc'].append(titulo_materia)
-        dicionario['link'].append(link_materia)
+        data_materia = sopa_data[0].getText()
+        if data_materia == data_ontem.strftime('%d/%m/%Y'):
+            dicionario['titulo'].append('Novo Informativo')
+            dicionario['desc'].append(titulo_materia)
+            dicionario['link'].append(link_materia)
 
-    dicionario['outstring'] = formata(dicionario, portal)
+        dicionario['outstring'] = formata(dicionario, portal)
+    except (requests.exceptions.ReadTimeout, requests.exceptions.SSLError,
+    requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError) as err:
+        lista_erros.append(err)
 
     return dicionario
 
@@ -74,28 +78,31 @@ def busca_sacha(portal, seletores):
 def busca_jota(portal, seletores):
     dicionario = {'titulo': [], 'desc': [], 'link': [], 'outstring': ''}
 
-    res = requests.get(portal['link'])
-    res.raise_for_status()
-
-    sopa = bs4.BeautifulSoup(res.text, 'html.parser')
-    sopa_titulo = sopa.select(seletores['titulo'])
-    sopa_desc = sopa.select(seletores['desc'])
-    sopa_link = sopa.select(seletores['link'])
-
-    for titulo, desc, link in zip(sopa_titulo, sopa_desc, sopa_link):
-        res = requests.get(link.get('href'))
+    try:
+        res = requests.get(portal['link'])
         res.raise_for_status()
 
         sopa = bs4.BeautifulSoup(res.text, 'html.parser')
-        sopa_data = sopa.select(seletores['data'])
+        sopa_titulo = sopa.select(seletores['titulo'])
+        sopa_desc = sopa.select(seletores['desc'])
+        sopa_link = sopa.select(seletores['link'])
 
-        dia_noticia = sopa_data[0].getText().strip()[:10]
-        if dia_noticia == data_ontem.strftime('%d/%m/%Y'):
-            dicionario['titulo'].append(titulo.getText().strip())
-            dicionario['desc'].append(desc.getText())
-            dicionario['link'].append(link.get('href'))
+        for titulo, desc, link in zip(sopa_titulo, sopa_desc, sopa_link):
+            res = requests.get(link.get('href'))
+            res.raise_for_status()
 
-    dicionario['outstring'] = formata(dicionario, portal)
+            sopa = bs4.BeautifulSoup(res.text, 'html.parser')
+            sopa_data = sopa.select(seletores['data'])
+
+            dia_noticia = sopa_data[0].getText().strip()[:10]
+            if dia_noticia == data_ontem.strftime('%d/%m/%Y'):
+                dicionario['titulo'].append(titulo.getText().strip())
+                dicionario['desc'].append(desc.getText())
+                dicionario['link'].append(link.get('href'))
+                dicionario['outstring'] = formata(dicionario, portal)
+    except (requests.exceptions.ReadTimeout, requests.exceptions.SSLError,
+    requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError) as err:
+        lista_erros.append(err)
 
     return dicionario
 
@@ -103,40 +110,44 @@ def busca_jota(portal, seletores):
 def busca_valor(portal, seletores):
     dicionario = {'titulo': [], 'desc': [], 'link': [], 'outstring': ''}
 
-    res = requests.get(portal['link'])
-    res.raise_for_status()
+    try:
+        res = requests.get(portal['link'])
+        res.raise_for_status()
 
-    sopa = bs4.BeautifulSoup(res.text, 'html.parser')
-    sopa_titulo = sopa.select(seletores['titulo'])
-    sopa_desc = sopa.select(seletores['desc'])
-    sopa_link = sopa.select(seletores['link'])
+        sopa = bs4.BeautifulSoup(res.text, 'html.parser')
+        sopa_titulo = sopa.select(seletores['titulo'])
+        sopa_desc = sopa.select(seletores['desc'])
+        sopa_link = sopa.select(seletores['link'])
 
-    for titulo, desc, link in zip(sopa_titulo, sopa_desc, sopa_link):
-        titulo = titulo.getText().strip()
-        desc = desc.getText().replace('\n','')
-        link = link.get('href')
+        for titulo, desc, link in zip(sopa_titulo, sopa_desc, sopa_link):
+            titulo = titulo.getText().strip()
+            desc = desc.getText().replace('\n','')
+            link = link.get('href')
 
-        if titulo not in dicionario['titulo']:
-            dicionario['titulo'].append(titulo)
-            link = 'https:' + link
+            if titulo not in dicionario['titulo']:
+                dicionario['titulo'].append(titulo)
+                link = 'https:' + link
 
-            res = requests.get(link)
-            res.raise_for_status()
-            sopa = bs4.BeautifulSoup(res.text, 'html.parser')
-            urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', str(sopa))
+                res = requests.get(link)
+                res.raise_for_status()
+                sopa = bs4.BeautifulSoup(res.text, 'html.parser')
+                urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', str(sopa))
 
-            res = requests.get(urls[0])
-            res.raise_for_status()
-            sopa = bs4.BeautifulSoup(res.text, 'html.parser')
-            nova_desc = sopa.select(seletores['nova_desc'])
+                res = requests.get(urls[0])
+                res.raise_for_status()
+                sopa = bs4.BeautifulSoup(res.text, 'html.parser')
+                nova_desc = sopa.select(seletores['nova_desc'])
 
-            dicionario['link'].append(urls[0])
-            if nova_desc:
-                dicionario['desc'].append(nova_desc[0].getText().replace('\n',''))
-            else:
-                dicionario['desc'].append('')
+                dicionario['link'].append(urls[0])
+                if nova_desc:
+                    dicionario['desc'].append(nova_desc[0].getText().replace('\n',''))
+                else:
+                    dicionario['desc'].append('')
 
-            dicionario['outstring'] = formata(dicionario, portal)
+                dicionario['outstring'] = formata(dicionario, portal)
+    except (requests.exceptions.ReadTimeout, requests.exceptions.SSLError,
+    requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError) as err:
+        lista_erros.append(err)
 
     return dicionario
 
@@ -144,20 +155,24 @@ def busca_valor(portal, seletores):
 def busca_supremo(portal, seletores):
     dicionario = {'titulo': [], 'desc': [], 'link': [], 'outstring': ''}
 
-    res = requests.get(portal['link'])
-    res.raise_for_status()
+    try:
+        res = requests.get(portal['link'])
+        res.raise_for_status()
 
-    sopa = bs4.BeautifulSoup(res.text, 'html.parser')
-    sopa_titulo = sopa.select(seletores['titulo'])
-    sopa_desc = sopa.select(seletores['desc'])
-    sopa_link = sopa.select(seletores['link'])
+        sopa = bs4.BeautifulSoup(res.text, 'html.parser')
+        sopa_titulo = sopa.select(seletores['titulo'])
+        sopa_desc = sopa.select(seletores['desc'])
+        sopa_link = sopa.select(seletores['link'])
 
-    for titulo, desc, link in zip(sopa_titulo, sopa_desc, sopa_link):
-        dicionario['titulo'].append(titulo.getText().strip())
-        dicionario['link'].append('http://portal.stf.jus.br' + link.get('href'))
-        dicionario['desc'].append(desc.getText().replace('\n',''))
+        for titulo, desc, link in zip(sopa_titulo, sopa_desc, sopa_link):
+            dicionario['titulo'].append(titulo.getText().strip())
+            dicionario['link'].append('http://portal.stf.jus.br' + link.get('href'))
+            dicionario['desc'].append(desc.getText().replace('\n',''))
 
-    dicionario['outstring'] = formata(dicionario, portal)
+        dicionario['outstring'] = formata(dicionario, portal)
+    except (requests.exceptions.ReadTimeout, requests.exceptions.SSLError,
+    requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError) as err:
+        lista_erros.append(str(err))
 
     return dicionario
 
@@ -174,11 +189,14 @@ def control(lista):
     else:
         outstring += noticias
 
-    """
-    print(outstring)
-    """
-    ezgmail.send('mendes.lnr@gmail.com','Clipping Tributário',outstring)
-    ezgmail.send('barreto.isabelaa@gmail.com','Clipping Tributário',outstring)
+    #print(outstring)
+    ezgmail.send('mendes.lnr@gmail.com', 'Clipping Tributário', outstring)
+
+
+    if lista_erros:
+        outerros = '\n'.join(lista_erros)
+        ezgmail.send('mendes.lnr@gmail.com', 'Erros Clipping', outerros)
+
 
 noticias_sacha = busca_sacha(sacha_dados, sacha_seletores)
 noticias_valor = busca_valor(valor_dados_1, valor_seletores)
